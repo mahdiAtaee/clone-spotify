cc99a8486a9b634288e4cff06b8b3cfcbaab24b3
mahdi <mahdiataee1689@gmail.com>
2023-09-28 20:39:22 +0330

    error message

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,6 +1,11 @@
 const Error = () => (
   <div className="flex justify-center items-center w-full">
-    <h1 className="font-bold text-2xl text-white mt-2">Something was wrong!</h1>
+    <h1 className="font-bold text-2xl text-white mt-2 text-center" dir="rtl">
+      Something was wrong!
+      <br />
+      لطفا از اتصال دستگاه خود به اینترنت مطمئن شوید در صورت متصل بودن از قندشکن استفاده کنید یا یک
+      دی ان اس (DNS) ست کنید
+    </h1>
   </div>
 )
 
